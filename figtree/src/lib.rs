56cbcf0c935f693a0a0b96b40183d1e87d8c681b
Andrei Merlescu <andrei+github@merlescu.net>
2026-05-17 23:57:39 -0400

    Update lib.rs

--- figtree/src/lib.rs
+++ figtree/src/lib.rs
@@ -1,5 +1,7 @@
 pub mod error;
 pub mod rules;
+pub mod fig;
 
 pub use error::{FigtreeError, FigtreeResult};
 pub use rules::Rule;
+pub use fig::{Fig, FigValue, FigSource, FigState, FigHistoryEntry};
